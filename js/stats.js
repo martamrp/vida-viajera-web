1d3c24f9a0a051e346691b5d877ef0597ccb4e9d
martamrp <martaromeroperez86@gmail.com>
2019-11-23 14:44:45 +0100

    Incluyo la opcion ver estadisticas

---
+++ js/stats.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    var userId = localStorage['userId'];
+
+    if (localStorage['userId'] === undefined) {
+        window.location.href = "login.html";
+    }
+    $("#back").click(function () {
+        window.location.href = "mostrar_datos.html";
+    });
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/users/" + userId + "/trips",
+        success: function (trips) {
+
+
+            $('#info1').text("El viaje más económico ha sido a ");
+
+
+        },
+        error: function () {
+            alert('Ha ocurrido un error inesperado');
+        }
+    });
+});
