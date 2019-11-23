118203d7ac3a4818cb6336c493cf855fe7ec9204
martamrp <martaromeroperez86@gmail.com>
2019-11-23 20:40:45 +0100

    obtengo el numero de viajes por ocio/trabajo

--- js/stats.js
+++ js/stats.js
@@ -17,10 +17,10 @@ $(document).ready(function () {
         type: "GET",
         url: "http://localhost:8080/users/" + userId + "/stats",
         success: function (stats) {
-          
-            $('#info1').text("El viaje más económico ha sido a "+stats.cheaperTrip.destination+" y ha costado "+stats.cheaperTrip.price+" euros.");
-            $('#info2').text("El viaje más caro ha sido a "+stats.moreExpensiveTrip.destination+" y ha costado "+stats.moreExpensiveTrip.price+" euros.");
 
+            $('#info1').text("El viaje más económico ha sido a " + stats.cheaperTrip.destination + " y ha costado " + stats.cheaperTrip.price + " euros.");
+            $('#info2').text("El viaje más caro ha sido a " + stats.moreExpensiveTrip.destination + " y ha costado " + stats.moreExpensiveTrip.price + " euros.");
+            $('#info3').text("Has hecho "+stats.leisureTrip+" viaje/s por ocio y "+stats.businessTrip+" viaje/s por trabajo.");
         },
         error: function () {
             alert('Ha ocurrido un error inesperado');
