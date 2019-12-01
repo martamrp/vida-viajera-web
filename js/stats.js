d4595496674819ecc7c9f2ef2815e142fd9ec71e
martamrp <martaromeroperez86@gmail.com>
2019-12-02 00:00:58 +0100

    Cuando un usuario no ha registrado ningun viaje, en lugar de las estadisticas le aparece un mensaje indicando que todavia no hay datos que mostrar

--- js/stats.js
+++ js/stats.js
@@ -17,48 +17,51 @@ $(document).ready(function() {
         type: "GET",
         url: Server + "/users/" + userId + "/stats",
         success: function(stats) {
+            if (stats.cheapestTrip == undefined) {
+                $('#noTrips').text('Todavía no has añadido ningún viaje. ¿Por qué no empiezas ahora?');
+            } else {
+                $('#statsList').append("<li>El viaje más económico ha sido a " + stats.cheapestTrip.destination + " y ha costado " + stats.cheapestTrip.price + " euros.</li>");
+                $('#statsList').append("<li>El viaje más caro ha sido a " + stats.mostExpensiveTrip.destination + " y ha costado " + stats.mostExpensiveTrip.price + " euros.</li>");
+                $('#statsList').append("<li>Has hecho " + stats.leisureTrips + " viaje/s por ocio y " + stats.businessTrips + " viaje/s por trabajo.</li>");
+                $('#statsList').append("<li>Tu viaje más corto ha sido a " + stats.shortestTrip.destination + " y a durado " + getTotalDays(new Date(stats.shortestTrip.startDate), new Date(stats.shortestTrip.endDate)) + " días.</li>");
+                $('#statsList').append("<li>Tu viaje más largo ha sido a " + stats.longestTrip.destination + " y a durado " + getTotalDays(new Date(stats.longestTrip.startDate), new Date(stats.longestTrip.endDate)) + " días.</li>");
+                var cheapestTripPerDay = stats.cheapestTripPerDay.price / getTotalDays(new Date(stats.cheapestTripPerDay.startDate), new Date(stats.mostExpensiveTripPerDay.endDate));
+                $('#statsList').append("<li>En proporción el viaje más barato que has hecho es " + stats.cheapestTripPerDay.destination +
+                    " con un coste de " + cheapestTripPerDay.toFixed(2) + "€/día.</li>");
+                var mostExpensiveTripPricePerDay = stats.mostExpensiveTripPerDay.price / getTotalDays(new Date(stats.mostExpensiveTripPerDay.startDate), new Date(stats.mostExpensiveTripPerDay.endDate));
+                $('#statsList').append("<li>En proporción el viaje más caro que has hecho es " + stats.mostExpensiveTripPerDay.destination +
+                    " con un coste de " + mostExpensiveTripPricePerDay.toFixed(2) + "€/día.</li>");
 
-            $('#info1').text("El viaje más económico ha sido a " + stats.cheaperTrip.destination + " y ha costado " + stats.cheaperTrip.price + " euros.");
-            $('#info2').text("El viaje más caro ha sido a " + stats.moreExpensiveTrip.destination + " y ha costado " + stats.moreExpensiveTrip.price + " euros.");
-            $('#info3').text("Has hecho " + stats.leisureTrip + " viaje/s por ocio y " + stats.businessTrip + " viaje/s por trabajo.");
-            $('#info4').text("Tu viaje más corto ha sido a " + stats.shortestTrip.destination + " y a durado " + getTotalDays(new Date(stats.shortestTrip.startDate), new Date(stats.shortestTrip.endDate)) + " días.");
-            $('#info5').text("Tu viaje más largo ha sido a " + stats.longestTrip.destination + " y a durado " + getTotalDays(new Date(stats.longestTrip.startDate), new Date(stats.longestTrip.endDate)) + " días.");
-            var cheapestTripPerDay = stats.cheapestTripPerDay.price / getTotalDays(new Date(stats.cheapestTripPerDay.startDate), new Date(stats.mostExpensiveTripPerDay.endDate));
-            $('#info6').text("En proporción el viaje más barato que has hecho es " + stats.cheapestTripPerDay.destination +
-                " con un coste de " + cheapestTripPerDay.toFixed(2) + "€/día.");
-            var mostExpensiveTripPricePerDay = stats.mostExpensiveTripPerDay.price / getTotalDays(new Date(stats.mostExpensiveTripPerDay.startDate), new Date(stats.mostExpensiveTripPerDay.endDate));
-            $('#info7').text("En proporción el viaje más caro que has hecho es " + stats.mostExpensiveTripPerDay.destination +
-                " con un coste de " + mostExpensiveTripPricePerDay.toFixed(2) + "€/día.");
+                $.ajax({
+                    type: "GET",
+                    url: Server + "/users/" + userId + "/countries",
+                    success: function(countryCodes) {
+                        $('#statsList').append("<li>Has estado en " + countryCodes.length + " países diferentes.</li>");
+
+                        $.ajax({
+                            type: "GET",
+                            url: Server + "/users/" + userId + "/regions",
+                            success: function(regions) {
+                                var text = "Has estado en un total de " + regions.length + " continentes diferentes:";
+                                regions.forEach(region => {
+                                    text += " " + region.name;
+                                });
+                                $('#statsList').append("<li>" + text + ".</li>");
+                                $('#statsList').append("<li>Y has recorrido el " + (countryCodes.length / 247 * 100).toFixed(2) + "% del mundo!</li>");
+                            },
+                            error: function() {
+                                alert('Ha ocurrido un error inesperado');
+                            }
+                        });
+                    },
+                    error: function() {
+                        alert('Ha ocurrido un error inesperado al obtener tus paises visitados');
+                    }
+                });
+            }
         },
         error: function() {
             alert('Ha ocurrido un error inesperado al obtener tus estadísticas');
-        }
-    });
-
-    $.ajax({
-        type: "GET",
-        url: Server + "/users/" + userId + "/countries",
-        success: function(countryCodes) {
-            $('#info8').text("Has estado en " + countryCodes.length + " países diferentes.");
-            $('#info10').text("Y has recorrido el " + (countryCodes.length / 247 * 100).toFixed(2) + "% del mundo!");
-        },
-        error: function() {
-            alert('Ha ocurrido un error inesperado al obtener tus paises visitados');
-        }
-    });
-
-    $.ajax({
-        type: "GET",
-        url: Server + "/users/" + userId + "/regions",
-        success: function(regions) {
-            var text = "Has estado en un total de " + regions.length + " continentes diferentes: ";
-            regions.forEach(region => {
-                text += region.name + " ";
-            });
-            $('#info9').text(text + ".");
-        },
-        error: function() {
-            alert('Ha ocurrido un error inesperado');
         }
     });
 });
